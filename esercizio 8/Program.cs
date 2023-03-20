e756300acb20ac5cee1d0d02edd1eb4f95912e73
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-03-20 13:28:04 +0100

    funzionamento

--- esercizio 8/Program.cs
+++ esercizio 8/Program.cs
@@ -8,13 +8,31 @@ namespace esercizio_8
 {
     internal class Program
     {
+        public struct Veicoli
+        {
+            public string nome;
+            public float km;
+            public float consumi;
+        }
+        public static Veicoli[] v;
+        public static int d;
         static void Main(string[] args)
         {
             
             Console.WriteLine("Inserire numero veicoli: ");
-
-            string[] a = new string[10];
-            Console.WriteLine("Inserire nome veicoli: ");
+            int d=int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < d; i++)
+            {
+                Console.WriteLine("Inserire nome veicoli: ");
+                v[i].nome = Console.ReadLine();
+                Console.WriteLine("Inserire chilometri percorsi: ");
+                v[i].km = float.Parse(Console.ReadLine());
+                Console.WriteLine("Inserire litri consumati: ");
+                v[i].consumi=float.Parse(Console.ReadLine());
+                int x = Convert.ToInt32(v[i].km / v[i].consumi);
+                Console.WriteLine("Consumo medio: " + x);
+            }
         }
     }
 }
